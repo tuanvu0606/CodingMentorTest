58c623673c68949b469ca51c7e13525b4f15379e
Nhat Tuan Vu <tuanvu0695@gmail.com>
2021-12-27 18:11:36 +1100

    added better solutions

--- pair_of_socks.py
+++ pair_of_socks.py
@@ -1,5 +1,11 @@
 def check_pair_of_socks():
-    # get the number of items from the user 
+    # This is the first solution that I could make \
+    # it contains 2 nested loops, first loop will iterate through the array, second loop
+    # will iterate from the index of first loop till the end of the array
+    # if it finds a matching sock, then turn both value at that index to be -1, out of the value range of ar[i] and will never be affecting the process.
+    # after that increment the pair_count
+    # as it finished the job, break the loop/
+    # its the nested loop so the time complexity is On^2, not very good
 
     ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
 
@@ -18,23 +24,24 @@ def check_pair_of_socks():
                     
                     print("paired")
                     break                      
-    print(pair_count)
+    return pair_count
 
 
 def check_pair_of_socks_v2():
-    # get the number of items from the user 
+    # This is the second solution which is faster with On^2 complexity, as 1 <= n <= 100 and 1<= ar[i] <= 100, we can define an array model_arr which has 101 element, the index will be the possible value of ar[i]
+    # then we loop through ar[i], for each of the value we will increment the value of model_arr, which will be used later to calculate the pair_count
+    # after that loop, we will loop again in model_arr, in each of the value, we divide it by 2 and add the rounded down integer to the pair_count.
+    
     pair_count = 0
     ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
 
-    b = [0] * 101
+    model_arr = [0] * 101
 
     for i in range(0, len(ar)):
-        b[ar[i]] += 1
-    for j in range(0, len(b)):
-        if b[j] > 0:
-            pair_count = pair_count + b[j]/2
+        model_arr[ar[i]] += 1
+    for j in range(0, len(model_arr)):
+        pair_count = pair_count + model_arr[j]//2
 
-    print(b)
-    print(pair_count)
+    return pair_count
 
 check_pair_of_socks_v2()
